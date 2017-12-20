392f56be3b2144f8b536927c72ead982acd8010e
Amin F <aminfaez@mail.upb.de>
2017-12-20 15:58:30 +0100

    Implemented storage.restore

--- src/pase/constants/error_msg.py
+++ src/pase/constants/error_msg.py
@@ -3,3 +3,7 @@ import const
 const.not_a_string = "{} is not a string."
 
 const.is_null = "{} is null."
+
+const.not_subtype = "{} is not of type {}."
+
+const.instance_with_id_doesnt_exist = "{} of type {} with id {} does not exist."
--- src/pase/store/storage.py
+++ src/pase/store/storage.py
@@ -4,32 +4,81 @@ import uuid
 import pase.constants.error_msg as error
 import jsonpickle
 
-def save(class_name, instance, id = None):
+def _topath(class_name):
+    return  class_name
 
-    # Boundary checks.
+def _tofilepath(class_name, id):
+    return class_name + "/" + id
+
+def _do_error_checks(class_name, instance, id = None):
+    """ Private function that checks if parameters are chosen without errors.
+    """
+     # Boundary checks.
     if(not isinstance(class_name, str) ):
-        raise ValueError(error.const.not_a_string.format("class_name"))
+        raise ValueError(error.const.not_a_string.format(f"class_name={class_name}")))
+
+    # The path to the instance.
+    path = _topath(class_name)
+    # Create folder if it doesnt exist.
+    os.makedirs(path, exist_ok = True)
+    # If id is given we need to check if the instance was saved before. (The storage module is the one who defines ids)
+    if id is not None:
+        if(not os.path.isfile(_tofilepath(class_name, id)):
+            raise ValueError(error.const.instance_with_id_doesnt_exist.format(f"{instance}", class_name, id))
+
+
+def save(class_name, instance, id = None):
+    """ save can be used to create or save an instance under its class name.
+    Params:
+    class_name: string of class name of instance with its full path. If Class Bar is in module bar in package foo class_name would be: "foo.bar.Bar"
+    instance: python instance which is to be saved.
+    id: Id of the instance to be saved. If the id is none, save creates a new id and returns it. Else save overwrites the file's old state.
+    
+    """
+    # Check for boundry errors.
+    _do_error_checks(class_name, instance, id)
     if(instance is None)
         raise ValueError(error.const.is_null.format("instance"))
+    if(not isinstance(instance, class_name)):
+        raise ValueError(error.const.not_subtype.format(f"{instance}", class_name)
 
     # The path to the instance.
     # Instances are stored in a subfolder named after the type's name.
-    path = class_name + "/"
-
-    # Create folder if it doesnt exist.
-    os.makedirs(path, exist_ok = True)
-
+    path = _topath(class_name)
+    
     # Generate id if id is not given.
     while id is None:
         id = uuid.uuid4().hex[:10].upper()
-        if(os.path.isfile(path + id)):
+        if(os.path.isfile(_tofilepath(class_name, id))):
             id = None
 
     # Finish the path.
-    path = path + id
+    path = _tofilepath(class_name, id)
+    # file pointer
+    # w+ overwrites the existing file if the file exists. If the file does not exist, creates a new file for reading and writing.
+    file = open(path, "w+") 
 
     # Serialize the object:
     jsonstring = jsonpickle.encode(instance)
 
+    file.write(jsonstring)
+    file.close()
+    
+    return id
+
+def restore(class_name, id):
+    # Boundary checks.
+    _do_error_checks(class_name, None, id)
+    # restore needs id to be assigend.
+    if(id == None): 
+        raise ValueError(error.const.is_null.format("id"))
+
+    path = _tofilepath(class_name,id)
     # file pointer
-    file = open(path, )
+    # r opens a file for reading only. 
+    file = open(path, "r") 
+    jsonstring = file.read()
+    file.close()
+    
+    instance = jsonpickle.decode(jsonstring)
+    return instance
