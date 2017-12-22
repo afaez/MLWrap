9c88c87a98740b3618e243176fc9564653fe658d
Amin F <aminfaez@mail.upb.de>
2017-12-22 13:06:09 +0100

    Small Bugfix in demo

--- src/demo_http_client.py
+++ src/demo_http_client.py
@@ -1,11 +1,11 @@
 from http.client import HTTPConnection
 import json
 
-body = json.dumps({"a" : 5 , "b" = 20})
+body = json.dumps({"a" : 5 , "b" : 20})
 head = {'Content-type': 'application/json'}
 
 h1 = HTTPConnection("localhost:5000")
-h1.request("POST", "/plainlib.package1.b.B", body, head))
+h1.request("POST", "/plainlib.package1.b.B", body, head)
 
 data = h1.getresponse().read()
 print(data)
